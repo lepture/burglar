563a2e1a07dec48abb08527b894d2690d1ad3056
Hsiaoming Yang <me@lepture.com>
2015-01-02 15:36:30 +0800

    Ready for work

--- burglar/__init__.py
+++ burglar/__init__.py
@@ -1,1 +1,31 @@
 # coding: utf-8
+
+import os
+from . import zhihu_daily, zhihu_zhuanlan, weixin
+from .utils import write_feed
+
+
+class Burglar(object):
+    def __init__(self, sitedir):
+        self.sitedir = sitedir
+
+    def __call__(self, item):
+        self.feed(item)
+
+    def feed(self, item):
+        assert 'type' in item
+        item_type = item['type']
+        if item_type == 'daily':
+            dest = os.path.join(self.sitedir, 'zhihu', 'daily.xml')
+            feed = zhihu_daily.parse()
+        elif item_type == 'zhuanlan':
+            name = item['name']
+            dest = os.path.join(self.sitedir, 'zhihu', name + '.xml')
+            feed = zhihu_zhuanlan.parse(item['title'], name)
+        elif item_type == 'weixin':
+            name = item['name']
+            dest = os.path.join(self.sitedir, 'weixin', name + '.xml')
+            feed = weixin.parse(item['title'], item['openid'])
+        else:
+            raise ValueError('Invalid item type')
+        write_feed(feed, dest)
--- burglar/utils.py
+++ burglar/utils.py
@@ -4,15 +4,28 @@ import os
 import json
 import logging
 import tempfile
+from requests.compat import bytes, str
 
 logger = logging.getLogger('burglar')
 
 
+def to_unicode(text):
+    if isinstance(text, str):
+        return text
+    return text.decode('utf-8')
+
+
+def to_bytes(text):
+    if isinstance(text, bytes):
+        return text
+    return text.encode('utf-8')
+
+
 def clean_cache(cache, keys):
-    for key in cache:
-        if key not in keys:
-            cache.pop(key)
-    return cache
+    rv = {}
+    for key in keys:
+        rv[key] = cache[key]
+    return rv
 
 
 def get_cache_file(cache_file, name):
@@ -34,7 +47,7 @@ def read_cache(cache_file):
 
 def write_cache(cache_file, cache, keys=None):
     if keys:
-        clean_cache(cache, keys)
+        cache = clean_cache(cache, keys)
 
     folder = os.path.dirname(cache_file)
     if folder and not os.path.isdir(folder):
@@ -44,37 +57,46 @@ def write_cache(cache_file, cache, keys=None):
         json.dump(cache, f)
 
 
-def _write_entry(entry):
-    yield '<entry>'
-    yield '<title><![CDATA[%s]]></title>' % entry['title']
-    yield '<link href="%s"/>' % entry['url']
-    yield '<id>%s</id>' % entry['url']
+def _iter_entry(entry):
+    yield u'<entry>'
+    yield u'<title><![CDATA[%s]]></title>' % to_unicode(entry['title'])
+    yield u'<link href="%s"/>' % to_unicode(entry['url'])
+    yield u'<id>%s</id>' % to_unicode(entry['url'])
 
     if 'author' in entry:
-        yield '<author><name>%s</name></author>' % entry['author']
+        yield u'<author><name>%s</name></author>' % to_unicode(entry['author'])
 
     if 'updated' in entry:
-        yield '<updated>%s</updated>' % entry['updated']
+        yield u'<updated>%s</updated>' % to_unicode(entry['updated'])
     if 'published' in entry:
-        yield '<published>%s</published>' % entry['published']
+        yield u'<published>%s</published>' % to_unicode(entry['published'])
 
-    yield '<content type="html"><![CDATA[%s]]></content>' % entry['body']
-    yield '</entry>'
+    yield u'<content type="html"><![CDATA[%s]]></content>' % \
+        to_unicode(entry['body'])
+    yield u'</entry>'
 
 
-def _write_feed(feed):
-    yield '<?xml version="1.0" encoding="utf-8"?>'
-    yield '<feed xmlns="http://www.w3.org/2005/Atom">'
-    yield '<title><![CDATA[%s]]></title>' % feed['title']
-    yield '<link href="%s" />' % feed['url']
-    yield '<id>%s</id>' % feed['url']
+def _iter_feed(feed):
+    yield u'<?xml version="1.0" encoding="utf-8"?>'
+    yield u'<feed xmlns="http://www.w3.org/2005/Atom">'
+    yield u'<title><![CDATA[%s]]></title>' % to_unicode(feed['title'])
+    yield u'<link href="%s" />' % to_unicode(feed['url'])
+    yield u'<id>%s</id>' % to_unicode(feed['url'])
     entries = feed['entries']
     item = entries[0]
-    yield '<updated>%s</updated>' % item['updated']
+    yield u'<updated>%s</updated>' % to_unicode(item['updated'])
     for entry in entries:
-        yield ''.join(list(_write_entry(entry)))
-    yield '</feed>'
+        yield u''.join(list(_iter_entry(entry)))
+    yield u'</feed>'
 
 
-def create_feed(feed):
-    return ''.join(list(_write_feed(feed)))
+def write_feed(feed, output):
+    content = ''.join(list(_iter_feed(feed)))
+
+    folder = os.path.dirname(output)
+    if folder and not os.path.isdir(folder):
+        os.makedirs(folder)
+
+    content = to_bytes(content)
+    with open(output, 'w') as f:
+        f.write(content)
