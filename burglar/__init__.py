40e0033a922a78e2c481ab0091a186948df242d8
Hsiaoming Yang <me@lepture.com>
2015-01-10 22:52:31 +0800

    use_cache as parameter for class Burglar

--- burglar/__init__.py
+++ burglar/__init__.py
@@ -18,35 +18,37 @@ class Burglar(object):
     """A burglar rob in the night.
 
     :param sitedir: directory for storing feed files.
+    :param use_cache: use cache for parsing or not.
     """
     PARSERS = ['daily', 'zhuanlan', 'weixin']
 
-    def __init__(self, sitedir):
+    def __init__(self, sitedir, use_cache=True):
         self.sitedir = sitedir
+        self.use_cache = use_cache
 
     def __call__(self, item):
         self.feed(item)
 
-    def parse_daily(self, item, use_cache=True):
-        feed = daily.parse(use_cache)
+    def parse_daily(self, item):
+        feed = daily.parse(self.use_cache)
         dest = os.path.join(self.sitedir, 'zhihu', 'daily.xml')
         return feed, dest
 
-    def parse_zhuanlan(self, item, use_cache=True):
+    def parse_zhuanlan(self, item):
         name = item['name']
-        feed = zhuanlan.parse(item['title'], name, use_cache)
+        feed = zhuanlan.parse(item['title'], name, self.use_cache)
         dest = os.path.join(self.sitedir, 'zhihu', name + '.xml')
         return feed, dest
 
-    def parse_weixin(self, item, use_cache=True):
+    def parse_weixin(self, item):
         name = item['name']
-        feed = weixin.parse(item['title'], item['openid'], use_cache)
+        feed = weixin.parse(item['title'], item['openid'], self.use_cache)
         if 'url' in item:
             feed['url'] = item['url']
         dest = os.path.join(self.sitedir, 'weixin', name + '.xml')
         return feed, dest
 
-    def feed(self, item, use_cache=True):
+    def feed(self, item):
         assert 'type' in item
         item_type = item['type']
         assert item_type in self.PARSERS
@@ -57,6 +59,6 @@ class Burglar(object):
 
         logger.info('Feeding %s - %s' % (item_type, name))
         parser = getattr(self, 'parse_%s' % item_type)
-        feed, dest = parser(item, use_cache)
+        feed, dest = parser(item)
         write_feed(feed, dest)
         logger.info('Finished %s - %s' % (item_type, name))
